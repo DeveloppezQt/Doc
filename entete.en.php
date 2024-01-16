a682c7f0ad26433c72445c28ff270be326f2e839
Thibaut Cuvelier <cuvelier.thibaut@gmail.com>
2024-01-16 01:13:17 +0100

    Initial commit for Qt 5.11.3 in English.

---
+++ entete.en.php
@@ -0,0 +1,20 @@
+<nav style="grid-column: span 2;">
+    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="background: white;">
+        <tbody>
+            <tr>
+                <td align="left">
+                    <a href="https://qt.developpez.com/doc/5.11/"><img src="https://qt.developpez.com/doc/images/qt_logo.gif" align="left" width="200" height="50" border="0" alt="Logo Documentation Qt"></a>
+                </td>
+                <td align="center">
+                    <strong style="font-size: 16px">Qt 5.11</strong>
+                </td>
+                <td align="center">
+                    ·&nbsp;
+                    <a href="https://qt.developpez.com/doc/5.11/reference-overview/">References</a> &nbsp;·&nbsp; 
+                    <a href="https://qt.developpez.com/doc/5.11/gettingstarted/">Getting Started</a> &nbsp;·&nbsp; 
+                    <a href="https://qt.developpez.com/doc/5.11/overviews/">Overviews</a> &nbsp;·&nbsp; 
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</nav>
